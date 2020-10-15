7e0b0251b214b78f6cfb119419e22e5afaeb254f
José León <jose.leon@branchcreation.com>
2020-10-15 10:55:51 +0200

    More changes

--- src/extension.ts
+++ src/extension.ts
@@ -7,24 +7,52 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	let api = {
 		getItems(): Array<any> {
-			let result: Array<any>=[];
+			let result: Array<any> = [];
 
 			result.push({
-				'icon': '',
+				'icon': './assets/images/pwa.svg',
 				'caption': 'Visual Studio Code Extension',
 				'description': 'Creates a new Visual Studio Code Extension',
 				'languages': ['TypeScript'],
 				'platforms': ['Linux', 'macOS', 'Windows', 'ARM'],
-				'types': ['Extension']
+				'types': ['Extension'],
+				'projectname': 'MyExtension',
+				'fields': [
+					{
+						"name": "identifier",
+						"label": "Extension identifier",
+						"type": "string"
+					},
+					{
+						"name": "description",
+						"label": "Description",
+						"type": "string"
+					},
+					{
+						"name": "initializegit",
+						"label": "Initialize git repository",
+						"type": "boolean"
+					},
+					{
+						"name": "packagemanager",
+						"label": "Package manager",
+						"type": "dropdown",
+						"options": [
+							"npm",
+							"yarn"
+						]
+					}
+				]
 			});
-	
+
 			result.push({
 				'icon': '',
 				'caption': 'Visual Studio Code Extension',
 				'description': 'Creates a new Visual Studio Code Extension',
 				'languages': ['JavaScript'],
 				'platforms': ['Linux', 'macOS', 'Windows', 'ARM'],
-				'types': ['Extension']
+				'types': ['Extension'],
+				'projectname': 'MyExtension'
 			});
 			return (result);
 		}
